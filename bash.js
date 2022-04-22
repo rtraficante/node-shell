07da2dc931de49143de032ace109e6e7d911b489
RTraficante <rtraficante22@gmail.com>
2022-04-22 12:43:29 -0400

    added pwd command

---
+++ bash.js
@@ -0,0 +1,11 @@
+const pwd = require("./pwd");
+
+process.stdout.write("prompt > ");
+
+process.stdout.on("data", (data) => {
+  const cmd = data.toString().trim();
+
+  if (cmd === "pwd") {
+    pwd();
+  }
+});
